90f345bcf58a2e1fd3999ae68ea19b34836f597e
ppaslan <91851698+ppaslan@users.noreply.github.com>
2022-11-23 17:52:07 +0100

    add pre commit

--- src/shop_app/profile.py
+++ src/shop_app/profile.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, render_template
 
-bp = Blueprint('start', __name__, url_prefix='/')
+bp = Blueprint("start", __name__, url_prefix="/")
 
 
 @bp.route("/")
 def index():
-    return render_template('profile.html')
+    """Returns main route"""
+    return render_template("profile.html")
