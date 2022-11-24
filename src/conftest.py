c10278d8a4662d44d1e6a05a17496c37f0512c47
ppaslan <91851698+ppaslan@users.noreply.github.com>
2022-11-24 10:50:27 +0100

    Set flask app to testing mode while testing

--- src/conftest.py
+++ src/conftest.py
@@ -40,7 +40,7 @@ def db_config(docker_ip, docker_services):
 
 @pytest.fixture()
 def test_client():
-    app = create_app()
+    app = create_app(**{"TESTING": True})
     with app.test_client() as test_client:
         with app.app_context():
             yield test_client
